20367c971292d5f62bdcf7a6fe304a984e72f5e2
kpngo1012 <220795382+kpngo1012@users.noreply.github.com>
2025-07-30 09:57:38 -0700

    Make all buttons functional and make sure UI corresponds with their respective functions
    "=": output the correct result and display on the input field
    while still letting users add more to the operation
    "clear": resets input field
    
    Other additions:
    Add space inbetween each input for better readibility and convenience

--- calculator.js
+++ calculator.js
@@ -16,7 +16,8 @@ function divide(num1, num2) {
 
 let num1, num2, operator;
 
-function operate(num1, num2, operator) {
+function operate(string) {
+  checkValidity(string);
   switch (operator) {
     case '+':
       return add(num1,num2);
@@ -29,7 +30,24 @@ function operate(num1, num2, operator) {
   }
 }
 
-// console.log(operate(1,2,'/'));
+//for testing purposes only
+// const opSymbols_test = ['+', '-', '*', '/', '=', '.'];
 
+function checkValidity(string) {
+  let arr = string.split(" ");
 
-// console.log(num1, num2, operator);
+  if (opSymbols.includes(arr[1]) && 
+    (arr[0].match(/\d/) && arr[2].match(/\d/))) {
+    num1 = parseInt(arr[0]);
+    operator = arr[1]
+    num2 = parseInt(arr[2]);
+    return true;
+  } else {
+    console.log("enter a valid operation in this format: # (operator) #");
+  }
+  
+  
+}
+
+// if (checkValidity("3 * 3")) { console.log(operate()) }
+// console.log(`Num1: ${num1} | Num2: ${num2} | Operator: ${operator}`);
--- interface.js
+++ interface.js
@@ -2,7 +2,9 @@ const numPad = document.querySelector("div");
 const operators = document.querySelector("div");
 const controls = document.querySelector("div");
 const display = document.querySelector("div");
+const inputField = document.querySelector("input");
 
+//numbers field
 for (let i = 0; i <= 9; i++) {
   const btn = document.createElement("button");
   btn.className = "num";
@@ -10,18 +12,47 @@ for (let i = 0; i <= 9; i++) {
   numPad.appendChild(btn);
 }
 
+//operator symbols
 const opSymbols = ['+', '-', '*', '/', '=', '.'];
 opSymbols.map(symbol => {
   const operator = document.createElement("button");
   operator.className = 'operators';
+  if (symbol === '=') operator.setAttribute("id", "equal");
   operator.textContent = symbol;
   operators.appendChild(operator);
 })
 
+//clear button - under controls 
 const clearBtn = document.createElement("button");
 clearBtn.className = 'clear';
 clearBtn.textContent = 'clear';
 controls.appendChild(clearBtn);
+
+let stringQuery = "";
+
+//allows display to populate as you click the btn
+for (const child of numPad.children) {
+  child.addEventListener("click", () => {
+    if (child.className === 'clear') { 
+      clearBtn.addEventListener("click", () => { 
+        inputField.value = ''});
+        stringQuery = "";
+      } else if (child.id === "equal") {
+        console.log(`Under child.id condition: ${inputField.value}`);
+        inputField.value = operate(inputField.value) + " ";
+        stringQuery = inputField.value;
+      } else {
+      inputField.value += child.textContent + ' ';
+      stringQuery = inputField.value;
+      console.log(`String Query: ${stringQuery}`);
+    }
+});
+}
+
+
+
+
+
 
 
 
